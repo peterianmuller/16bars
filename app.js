eba36800f32847931825a28a7f2082752efec8bd
svey <marcus.svehlak@gmail.com>
2017-09-09 18:35:38 -0700

    Added index.html and app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    <div>Working</div>
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
